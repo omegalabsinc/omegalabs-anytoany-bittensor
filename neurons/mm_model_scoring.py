1cf66736fa374142ce932175939c3266b0ab06d1
Xzistance <xzistance3@gmail.com>
2024-09-08 18:48:58 +0000

    adding llama3 caption generating function

--- neurons/mm_model_scoring.py
+++ neurons/mm_model_scoring.py
@@ -151,6 +151,55 @@ def get_caption_from_model(hf_repo_id, video_emb):
     
     return generated_caption
 
+def get_caption_from_llama_31(hf_repo_id, video_emb):
+    hf_api = huggingface_hub.HfApi()
+    ckpt_files = [f for f in hf_api.list_repo_files(repo_id=hf_repo_id) if f.startswith(MODEL_FILE_PREFIX)]
+    if len(ckpt_files) == 0:
+        raise ValueError(f"No checkpoint files found in {hf_repo_id}")
+
+    # Create a unique subdirectory for each repository within the cache directory
+    repo_cache_dir = os.path.join(CACHE_DIR, hf_repo_id.replace("/", "_"))
+    os.makedirs(repo_cache_dir, exist_ok=True)
+
+    # Define paths for the config and checkpoint files
+    config_path = os.path.join(repo_cache_dir, CONFIG_FILE)
+    ckpt_path = os.path.join(repo_cache_dir, ckpt_files[0])
+
+    # Check if the files are outdated
+    if is_file_outdated(config_path, CACHE_EXPIRY_HOURS) and refresh_cache:
+        if os.path.exists(config_path):
+            os.remove(config_path)
+        config_path = hf_api.hf_hub_download(repo_id=hf_repo_id, filename=CONFIG_FILE, local_dir=repo_cache_dir)
+
+    if is_file_outdated(ckpt_path, CACHE_EXPIRY_HOURS) and refresh_cache:
+        if os.path.exists(ckpt_path):
+            os.remove(ckpt_path)
+        ckpt_path = hf_api.hf_hub_download(repo_id=hf_repo_id, filename=ckpt_files[0], local_dir=repo_cache_dir)
+
+    train_cfg = OmegaConf.load(config_path)
+    train_cfg.model = DictConfig({
+        "_component_": "models.mmllama3_8b",
+        "use_clip": False,
+        "perception_tokens": train_cfg.model.perception_tokens,
+    })
+    train_cfg.checkpointer.checkpoint_dir = os.path.dirname(ckpt_path)
+    train_cfg.checkpointer.checkpoint_files = [os.path.basename(ckpt_path)]
+    train_cfg.inference.max_new_tokens = 300
+    train_cfg.tokenizer.path = "./models/tokenizer.model"
+    
+    inference_recipe = InferenceRecipe(train_cfg)
+    inference_recipe.setup(cfg=train_cfg)
+
+    inference_recipe, train_cfg = load_ckpt_from_hf_cached(hf_repo_id)
+    prompt = "Generate a caption for this video"
+    generated_caption = inference_recipe.generate_from_any(cfg=train_cfg, prompt=prompt, embeddings=[{"video": video_emb}])
+
+    # Unload model from GPU memory
+    del inference_recipe
+    torch.cuda.empty_cache()
+    
+    return generated_caption
+
 def get_mm_response(hf_repo_id, prompt, embeddings, assistant = ""):
     refresh_cache = True
     if hf_repo_id == "briggers/omega_a2a_test4":
