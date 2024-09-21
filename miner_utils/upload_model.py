d91dc4060fbeaaf8799cb8a29263b09e02f22edd
Xzistance <165348230+xzistance@users.noreply.github.com>
2024-09-21 08:09:14 -0500

    checking for hotkey.txt in miner's HF repo for the contents of their … (#24)
    * checking for hotkey.txt in miner's HF repo for the contents of their associated hotkey. if hotkey being scored doesn't match, score 0
    
    * adding in a hotkey.txt file to be uploaded to the model on HF for cross-checking during scoring.

--- miner_utils/upload_model.py
+++ miner_utils/upload_model.py
@@ -130,6 +130,7 @@ async def main(config: bt.config):
     model_id_with_commit = await remote_model_store.upload_model(
         model=model,
         competition_parameters=parameters,
+        hotkey=wallet.hotkey.ss58_address
     )
 
     model_id_with_hash = ModelId(
@@ -152,13 +153,13 @@ async def main(config: bt.config):
     # We can only commit to the chain every n minutes, so run this in a loop, until successful.
     while True:
         try:
+            await model_metadata_store.store_model_metadata(
+                wallet.hotkey.ss58_address, model_id_with_hash
+            )
             update_repo_visibility(
                 model_id.namespace + "/" + model_id.name,
                 private=False,
                 token=os.getenv("HF_ACCESS_TOKEN"),
-            )
-            await model_metadata_store.store_model_metadata(
-                wallet.hotkey.ss58_address, model_id_with_hash
             )
             bt.logging.success("Committed model to the chain.")
             break
