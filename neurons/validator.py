151c370d993aa4efa41ef31278889b747c069130
Tezuesh Varshney <tezuesh.varshney@gmail.com>
2025-01-28 14:37:49 +0000

    Hotfix: MINAGE

--- neurons/validator.py
+++ neurons/validator.py
@@ -438,7 +438,7 @@ class Validator:
         block_uploaded_at = model_metadata.block
         current_block = self.metagraph.block.item()
         model_age = (current_block - block_uploaded_at) * constants.BLOCK_DURATION
-        is_old_enough = model_age > constants.O1_MIN_AGE if model_metadata.id.competition_id == "o1" else model_age > constants.V2V_MIN_AGE
+        is_old_enough = model_age > constants.MIN_AGE
         if not is_old_enough:
             bt.logging.debug(
                 f"Model {model_metadata.id} is too new to evaluate. Age: {model_age} seconds"
