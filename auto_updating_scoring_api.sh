2692219f402651c27ad66f6d0f690afe5834b57b
Siddhant Verma <54078398+siddhantoon@users.noreply.github.com>
2025-08-12 23:57:45 +0530

    Update auto_updating_scoring_api.sh

--- auto_updating_scoring_api.sh
+++ auto_updating_scoring_api.sh
@@ -8,5 +8,7 @@ git pull
 # next, set up environment
 pip install -e .
 
+python -m nltk.downloader punkt punkt_tab
+
 # finally, run the scoring api
 python neurons/scoring_api.py $API_ARGS --auto_update
